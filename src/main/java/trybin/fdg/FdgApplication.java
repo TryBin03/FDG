20b19f261e053cdf9f84f960bc41a0bf79a7fa5b
tryBin03 <try2029138116@163.com>
2021-10-27 17:05:22 +0800

    COMMIT

---
+++ src/main/java/trybin/fdg/FdgApplication.java
@@ -0,0 +1,16 @@
+package trybin.fdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TryBin
+ */
+@SpringBootApplication
+public class FdgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trybin/fdg/FdgApplicationTests.java
@@ -0,0 +1,13 @@
+package trybin.fdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
