54b41706ef2fa318c21dbcb06dd4de55dca520d3
TryBin03 <2029138116@qq.com>
2021-11-09 23:17:07 +0800

    Sql文

---
+++ sql/MySQL_sql.sql
@@ -0,0 +1,33 @@
+SELECT
+    COLUMN_NAME                                                        AS COLNAME,
+    IF(COLUMN_KEY = 'PRI',1,NULL)                                         AS KEYSEQ,
+    DATA_TYPE                                                             AS TYPENAME,
+    CAST(IFNULL(CHARACTER_MAXIMUM_LENGTH,NUMERIC_PRECISION) AS UNSIGNED )    LENGTH
+FROM
+    information_schema.columns
+WHERE
+    table_schema = 'fdgtest'
+AND table_name = 'test_1'
+ORDER BY
+    ORDINAL_POSITION
+--查询所有表
+SELECT DISTINCT
+    CONCAT_WS('.', TABLE_SCHEMA, TABLE_NAME) TABLEID,
+    TABLE_NAME                               TABLENAME
+FROM
+    information_schema.columns
+WHERE
+    table_schema NOT IN ('information_schema',
+                         'mysql',
+                         'performance_schema')
+--查询所有列
+SELECT
+    CONCAT_WS('.', TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME) COLUMNID,
+    COLUMN_NAME                                           COLUMNNAME,
+    DATA_TYPE
+FROM
+    information_schema.columns
+WHERE
+    table_schema NOT IN ('information_schema',
+                         'mysql',
+                         'performance_schema')
---
+++ sql/Oracle_sql.sql
@@ -0,0 +1,38 @@
+SELECT
+    utc.COLUMN_NAME                          AS COLUMN_NAME,
+    DECODE(uc.constraint_type, 'P', 1, NULL) AS KEYSEQ,
+    DATA_TYPE                                AS TYPENAME,
+    DATA_LENGTH                              AS LENGTH
+FROM
+    user_tab_columns utc
+LEFT JOIN
+    user_cons_columns ucc
+ON
+    utc.TABLE_NAME = ucc.TABLE_NAME
+AND utc.COLUMN_NAME = ucc.COLUMN_NAME
+LEFT JOIN
+    user_constraints uc
+ON
+    ucc.constraint_name = uc.constraint_name
+WHERE
+    utc.TABLE_NAME = 'test_1'
+AND uc.index_name IS NOT NULL
+OR  (
+        utc.TABLE_NAME = 'test_1'
+    AND uc.index_name IS NULL
+    AND ucc.constraint_name IS NULL)
+--查询所有表   DEV 为用户名
+SELECT
+    OWNER||'.'||TABLE_NAME  TABLEID,
+    TABLE_NAME                               TABLENAME
+FROM
+    user_constraints
+WHERE INDEX_OWNER = 'DEV'
+
+--查询所有列    DEV 为用户名
+SELECT
+    'DEV'||'.'||TABLE_NAME||'.'||COLUMN_NAME COLUMNID,
+    COLUMN_NAME                                           COLUMNNAME,
+    DATA_TYPE
+FROM
+    user_tab_columns
